96e466490f41642fbde2d746e3678bab13f73098
Agustin <ageus94@gmail.com>
2023-06-02 16:58:07 -0600

    little change

--- fisica_new.py
+++ fisica_new.py
@@ -4,21 +4,6 @@ import datetime
 import pathlib
 import os
 
-ROOT_PATH = pathlib.Path().resolve()                                    # Directorio raíz
-FIEL_PATH = ROOT_PATH.__str__() + "\\FIEL\\"                            # Directorio donde estan las carpetas de las fieles
-SAVE_PATH = ROOT_PATH.__str__() + "\\SAVE_FISICA_OLD\\"                            # Directorio donde se guardaran los pdfs
-ERRORS_PATH = ROOT_PATH.__str__() + "\\ERRORS\\"                        # Directorio donde se guardaran los errores
-CURRENT_MONTH = datetime.datetime.now().month                           # El mes actual
-CURRENT_YEAR = datetime.datetime.now().year
-
-already_downloaded = [getRFCfromTopDirectory(f, '.') for f in os.listdir(SAVE_PATH)]
-with_error = [getRFCfromTopDirectory(f, '.') for f in os.listdir(ERRORS_PATH)]
-already_downloaded = already_downloaded + with_error
-print(already_downloaded)
-fiel_folders = [d for d in os.listdir(FIEL_PATH) if os.path.isdir(os.path.join(FIEL_PATH, d))]  # Nombre de la carpeta del empresario
-remaining = [rfc for rfc in fiel_folders if getRFCfromTopDirectory(rfc, '_') not in already_downloaded] # Los que faltan por generar
-
-
 def ir_a(pagina=""):
     driver = webdriver.Chrome()
     driver.maximize_window()
@@ -66,9 +51,29 @@ def getCredentials(fiel_path="", fiel_folder="", save_path=""):
     password = get_pass(fiel_path, fiel_folder) 
     return ruta_certificado, ruta_clave_privada, password
 
+ROOT_PATH = pathlib.Path().resolve()                                    # Directorio raíz
+FIEL_PATH = ROOT_PATH.__str__() + "\\FIEL\\"                            # Directorio donde estan las carpetas de las fieles
+SAVE_PATH = ROOT_PATH.__str__() + "\\SAVE_FISICA_NEW\\"                            # Directorio donde se guardaran los pdfs
+ERRORS_PATH = ROOT_PATH.__str__() + "\\ERRORS_NEW\\"                        # Directorio donde se guardaran los errores
+CURRENT_MONTH = datetime.datetime.now().month                           # El mes actual
+CURRENT_YEAR = datetime.datetime.now().year
+
+already_downloaded = [getRFCfromTopDirectory(f, '.') for f in os.listdir(SAVE_PATH)]
+with_error = [getRFCfromTopDirectory(f, '.') for f in os.listdir(ERRORS_PATH)]
+already_downloaded = already_downloaded + with_error
+print(already_downloaded)
+fiel_folders = [d for d in os.listdir(FIEL_PATH) if os.path.isdir(os.path.join(FIEL_PATH, d))]  # Nombre de la carpeta del empresario
+remaining = [rfc for rfc in fiel_folders if getRFCfromTopDirectory(rfc, '_') not in already_downloaded] # Los que faltan por generar
+
 
 for fiel_folder in remaining :
-    
+
     mydriver = ir_a("https://pstcdypisr.clouda.sat.gob.mx/")
 
     ruta_certificado, ruta_clave_privada, password = getCredentials(fiel_path=FIEL_PATH, fiel_folder=fiel_folder, save_path=SAVE_PATH) 
+
+    print(ruta_certificado)
+
+    print(ruta_clave_privada)
+
+    print(password)
